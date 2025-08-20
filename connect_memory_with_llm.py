2823497782fcb588d3b4b2cc113bec46e30221ab
Rumaisa <105513477+Rumaisa1054@users.noreply.github.com>
2025-08-21 01:27:48 +0500

    Update connect_memory_with_llm.py

--- connect_memory_with_llm.py
+++ connect_memory_with_llm.py
@@ -24,7 +24,7 @@ from langchain_community.vectorstores import FAISS
 HUGGINGFACE_REPO_ID="mistralai/Mistral-7B-Instruct-v0.3"
 
 def load_llm(huggingface_repo_id):
-    groq_llm=ChatGroq(model="mixtral-8x7b-32768",api_key="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")
+    groq_llm=ChatGroq(model="mixtral-8x7b-32768",api_key="GROK_API_HERE")
     return groq_llm
 
 # Step 2: Connect LLM with FAISS and Create chain
@@ -68,3 +68,4 @@ ch = 'y'
 while(ch != 'n'):
     print(response(input("Query : ")))
     ch = input("continue")
+
